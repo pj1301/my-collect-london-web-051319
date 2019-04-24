e47476cd475c8f6fac6eeb36e958f8c0d2957e6c
James Hayward <james.hayward6@icloud.com>
2019-04-24 08:53:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,4 @@
- # require_relative './spec/my_collect_spec.rb'
 
- # languages = ['ruby', 'javascript', 'python', 'objective-c']
- # students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
-
-# def my_collect(array)
-#   i = 0
-#   while i < array.length
-#     yield(array[i])
-#     i += 1
-#   end
-# end
-#
-#  my_collect(collection) {|language| language.upcase}
-#
-#  my_collect(student) {|name| name.split(" ").first}
-#
 def my_collect(array)
   i = 0
   collection = []
