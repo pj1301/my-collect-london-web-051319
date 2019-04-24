79c64fdead9893366004c8ad7568465a1ce6ede5
James Hayward <james.hayward6@icloud.com>
2019-04-24 08:54:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 
-def my_collect(array)
-  i = 0
-  collection = []
+def my_collect(array) #defines the function and draws on the specific array
+  i = 0 #starts and iterative loop
+  collection = [] #creates an empty collection which can receive new values as per the instruction to return a new collection
   while i < array.length
     collection << yield(array[i])
     i += 1
