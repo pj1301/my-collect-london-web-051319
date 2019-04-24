aa94a8dfeb0d94b3c12c73b393acdb4d0eca0377
James Hayward <james.hayward6@icloud.com>
2019-04-24 08:04:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,11 @@
+require_relative './spec/my_collect_spec.rb'
+
 def my_collect(collection)
   i = 0
   while i < collection.length
-    yield collection[i]
+    yield(collection[i])
     i += 1
   end
 end
+
+my_collect(collection) {|collection| collection.upcase}
